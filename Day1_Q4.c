fe637321aa643d3def26091d025d771b83162171
amanchaurasiya711-tech <amanchaurasiya711@gmail.com>
2026-05-24 12:15:50 +0530

    day-1 question-4

--- Day1_Q4.c
+++ Day1_Q4.c
@@ -3,17 +3,17 @@
 #include<stdio.h>
 int main()
 {
-    int Num,count=0;
+    int a,Num,count=0;
     
     printf("Enter the number for which you want to count the digits: ");
     scanf("%d",&Num);
-
+    a=Num;
     while(Num!=0)
     {
     Num/=10;
     count++;
     }
 
-    printf("The number of digits in %d is: %d",Num,count);
+    printf("The number of digits in %d is: %d",a,count);
     return 0;
 }
