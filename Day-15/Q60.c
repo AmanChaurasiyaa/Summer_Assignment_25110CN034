fa3baff88d00a0529f830717478f76de67382167
amanchaurasiya711-tech <amanchaurasiya711@gmail.com>
2026-06-15 09:11:08 +0530

    day 15 completed

--- Day-15/Q60.c
+++ Day-15/Q60.c
@@ -1,1 +1,40 @@
 //Q60 Write a program to Move zeroes to end. 
+
+#include<stdio.h>
+
+int main()
+{
+    int n, i, j = 0;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    // Move zeroes to end
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] != 0)
+        {
+            arr[j++] = arr[i];
+        }
+    }
+
+    while(j < n)
+    {
+        arr[j++] = 0;
+    }
+    
+    printf("Array after moving zeroes to end: ");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    
+    return 0;
+}
